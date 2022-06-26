5a875b253154930a070f05f4b849c45dad7ed91f
Eduardo Serafim <eduserafim7@gmail.com>
2022-06-25 22:42:35 -0300

    Classe Position

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,13 @@
 package app;
 
+import boardGame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Chess game");
-
+		Position position = new Position(3, 5);
+		System.out.println(position);
 	}
 
 }
