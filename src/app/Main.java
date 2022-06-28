2f4fd1a9f9db4cfddf3a5154c6130b32791d1d59
Eduardo Serafim <eduserafim7@gmail.com>
2022-06-28 19:51:00 -0300

    Colcando peças no tabuleiro

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		UI.printBoard (chessMatch.getPieces());
 	}
 
 }
