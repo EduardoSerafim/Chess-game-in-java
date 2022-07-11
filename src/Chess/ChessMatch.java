21b3ca0b916c8854c12740f547cc5f88b8c9c28c
Eduardo Serafim <eduserafim7@gmail.com>
2022-07-10 23:52:19 -0300

    Correção no tratamento da promoção

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -122,8 +121,9 @@ public class ChessMatch {
 		if(promoted == null) {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
+		
 		if(!type.equals("B") && !type.equals("R") && !type.equals("N") && !type.equals("Q") ) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
